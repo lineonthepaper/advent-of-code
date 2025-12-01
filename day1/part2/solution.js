2f8eb9231e5f7992177353711bad60d9ff1e6cb7
lineonthepaper <lineonrl@gmail.com>
2025-12-01 19:01:53 +0800

    day 1 part 2
    (bruteforced :( to revisit again next time)

---
+++ day1/part2/solution.js
@@ -0,0 +1,39 @@
+
+function process() {
+    let input = document.getElementById("input").value;
+    let splitInput = input.split("\n");
+    
+    let current = 50;
+    let result = 0;
+
+    for (let line of splitInput) {
+        [current, result] = rotate(line.substring(0, 1), Number(line.substring(1)), current, result);
+    }
+    
+    let output = document.getElementById("output");
+    output.innerText = result;
+}
+
+function rotate(direction, value, current, result) {
+    let numTimes = 0;
+    for (let i = 0; i < value; i++) {
+        if (direction === "L") {
+            current--;
+        }
+        else {
+            current++;
+        }
+        current %= 100;
+        if (current === 0) {
+            numTimes++;
+        }
+    }
+
+    // console.log(direction + value);
+    // console.log("Now points at: " + current);
+    // console.log("Num times passed 0: " + numTimes);
+    // console.log("-----");
+
+    return [current, result + numTimes];
+    
+}
