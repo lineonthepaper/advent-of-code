a93d426dcef63ab162073c9355fa619013090569
lineonthepaper <lineonrl@gmail.com>
2025-12-02 19:49:37 +0800

    day 2 part 1

---
+++ day2/js/part1/solution.js
@@ -0,0 +1,29 @@
+function process() {
+    let input = document.getElementById("input").value;
+    let splitInput = input.split(",");
+    
+    let result = 0;
+
+    for (let line of splitInput) {
+        let [start, end] = line.split("-");
+        result += findInvalid(start, end);
+    }
+
+    let output = document.getElementById("output");
+    output.innerText = result;
+}
+
+function findInvalid(start, end) {
+    let sum = 0;
+    for (let i = Number(start); i <= Number(end); i++) {
+        let numString = i + "";
+        if (numString.length % 2 !== 0) {
+            continue;
+        }
+        if (numString.substring(0, numString.length / 2) === numString.substring(numString.length / 2)) {
+            sum += i;
+        }
+    }
+
+    return sum;
+}
